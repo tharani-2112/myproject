6ec6c34f5b1a6053d83e53f521707a7be0d82189
Tharani Harichandran <tharani.harichandran@aspiresys.com>
2021-06-28 10:22:18 +0530

    html validation

---
+++ 25june2021/registration2.js
@@ -0,0 +1,10 @@
+function Register()
+{
+    var name=document.getElementById("FullName");
+    var email=document.getElementById("UserName");
+    var gender=document.getElementById("Gender");
+    var password=document.getElementById("Password");
+    var Address = document.getElementById("Address");
+    alert("Details entered successfully");
+
+}
