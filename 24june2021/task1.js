094bdd94903cffaf6baa6977db78d9bcf899c3c3
Tharani Harichandran <tharani.harichandran@aspiresys.com>
2021-06-28 10:14:56 +0530

    Html form validation

---
+++ 24june2021/task1.js
@@ -0,0 +1,16 @@
+//var firstname="tharani";
+//alert(firstname);
+//alert(777);
+//alert(firstname + " likes choclate ");
+//alert(5+6);
+//document.write(12+3);
+//var x=100;
+//var y=200;
+//document.write(x+y);
+//alert("tharani".length);
+document.writeln('It\'s 6\'o clock');
+document.writeln ("  I know   tamil  ".trim());
+function myfun()
+{
+    document.getElementById("demo").innerHTML=Date();
+}
